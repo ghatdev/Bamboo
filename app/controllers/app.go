aeef66b7570cca58b4111f82bb5f70fe7228d19a
SeongUk Cho <issacs98@gmail.com>
2016-09-08 23:11:56 +0900

    질문 업데이트

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -19,8 +19,10 @@ type App struct {
 func (c App) Index() revel.Result {
 	question := [3]string{
 		"2016년 학생회장이 선출된 이후 이 페이지를 개발한 누구는 공식적으로 이 별명을 얻게되었다. 이 별명은?(3글자)",
-		"충남삼성고등학교 학생생활부장 선생님의 성함은?",
-		"영어선생님 중 노래도 잘하시고 드럼도 잘치시는 선생님이 있다. 이 선생님의 성함은?"} //재학생 확인 질문 목록
+		"학교 내부 인트라넷 큰사넷의 내부 접속 주소 마지막 . 뒤의 숫자는? (***.***.***.여기)",
+		"영어선생님 중 노래도 잘하시고 드럼도 잘치시는 선생님이 있다. 이 선생님의 성함은?",
+		"학교의 모든 급식에 등장하는 유명한 이것은?",
+		"면학시간을 칭하는 명칭 중 유일하게 주말과 평일 모두 존재하는 면학시간의 명칭은? (2글자, 대문자로)"} //재학생 확인 질문 목록
 
 	s := rand.NewSource(time.Now().UnixNano()) // 랜덤 시드를 시간 기반으로 설정
 	r := rand.New(s)                           // 바뀐 랜덤 시드로 r 생성
@@ -48,7 +50,7 @@ func (c App) Post(answer string, message string, qnum int, snum string) revel.Re
 		return c.Redirect(App.Index)
 	}
 
-	qanswer := [3]string{"불효자", "손병희", "손재식"} //재학생 질문 확인 답. 질문번호와 답 번호가 같다
+	qanswer := [3]string{"불효자", "32", "손재식", "방울토마토", "EP"} //재학생 질문 확인 답. 질문번호와 답 번호가 같다
 
 	if !strings.Contains(answer, qanswer[qnum]) { // 전달받은 질문 번호의 답을 포함하지 않을경우
 		c.Flash.Error("재학생 확인 질문에 대한 답이 잘못되었습니다.") // 오류 메세지
